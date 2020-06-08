69478ceed116ae517aaea2bf38b1ccc24a20d9f6
Seungmin <hsm012362@gmail.com>
2020-06-08 14:43:54 +0900

    project init

---
+++ CatchMyMind_backend/src/main/java/com/min/catchmymind/CatchMyMind_backend/CatchMyMindBackendApplication.java
@@ -0,0 +1,13 @@
+package com.min.catchmymind.CatchMyMind_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchMyMindBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchMyMindBackendApplication.class, args);
+	}
+
+}
---
+++ CatchMyMind_backend/src/test/java/com/min/catchmymind/CatchMyMind_backend/CatchMyMindBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.min.catchmymind.CatchMyMind_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchMyMindBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
